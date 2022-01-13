a4eae3c8a25fa325a42b0d9ab1a7d424dc0b1591
Anshuljain05 <60061779+Anshuljain05@users.noreply.github.com>
2022-01-13 19:31:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+let display = document.getElementById('display');
+let buttons = Array.from(document.getElementsByClassName('button'));
+buttons.map( button => {
+    button.addEventListener('click', (e) => {
+        if(e.target.id == 'back-icon'){
+            if (display.innerText){
+                display.innerText = display.innerText.slice(0, -1);
+            }
+        }
+        else if(e.target.id == 'star-icon'){
+            console.log('Favourite Button Clicked!');
+        }
+        else if(e.target.id == 'call-icon'){
+            console.log('Call Button Clicked!');
+        }
+        else {
+            let currentButtonText = e.target.innerText;
+            let firstValue = currentButtonText[0];
+            if(firstValue == '2' || firstValue == 'A'){
+                display.innerText += '2';
+            }
+            else if(firstValue == '3' || firstValue == 'D'){
+                display.innerText += '3';
+            }
+            else if(firstValue == '4' || firstValue == 'G'){
+                display.innerText += '4';
+            }
+            else if(firstValue == '5' || firstValue == 'J'){
+                display.innerText += '5';
+            }
+            else if(firstValue == '6' || firstValue == 'M'){
+                display.innerText += '6';
+            }
+            else if(firstValue == '7' || firstValue == 'P'){
+                display.innerText += '7';
+            }
+            else if(firstValue == '8' || firstValue == 'T'){
+                display.innerText += '8';
+            }
+            else if(firstValue == '9' || firstValue == 'W'){
+                display.innerText += '9';
+            }
+            else{
+                display.innerText += firstValue;
+            }  
+        }
+    });
+});
